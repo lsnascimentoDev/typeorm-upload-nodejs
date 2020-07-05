43f25ed6fdaf789e07062c18820af50f5b7b897a
Lucas Nascimento <lnascimento.dev@gmail.com>
2020-07-05 18:52:27 -0300

    finalized

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,24 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import Transaction from './Transaction';
+
+@Entity('categories')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,23 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import TransactionRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+
+    const transactionExists = await transactionRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!transactionExists) {
+      throw new AppError('Transaction does not exists');
+    }
+
+    await transactionRepository.remove(transactionExists);
   }
 }
 
